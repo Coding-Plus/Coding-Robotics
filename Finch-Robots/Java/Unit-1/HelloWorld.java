68a5b2be860fc2017a3a017664bb0fd7ba9a0643
Derek Leung <derex.leung@gmail.com>
2016-01-19 18:37:08 -0500

    Update HelloWorld.java

--- Finch-Robots/Java/Unit-1/HelloWorld.java
+++ Finch-Robots/Java/Unit-1/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld
 {
   public static void main(String[] args) //Don't worry about this syntax for now!  We will cover this in later units.
       {
-        //Prints "Hello World" to the terminal window.
+        //Prints "Hello World!" to the terminal window.
         System.out.println("Hello World!")
         //Within the parenthesis and quotations, you can change the the phrase to display whatever you want!  Try saying "Hello World!  My name is [...]"
         }
