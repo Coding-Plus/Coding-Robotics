178eb8bb0f4f570e1f35949ac22bb1086ee7b1dd
Derek Leung <derex.leung@gmail.com>
2016-01-19 18:30:02 -0500

    Update HelloWorld.java

--- Finch-Robots/Java/Unit-1/HelloWorld.java
+++ Finch-Robots/Java/Unit-1/HelloWorld.java
@@ -1,11 +1,17 @@
-//Welcome to Java!  Java is a powerful coding language that has many uses from coding apps to robots to medical devices.  In this tutorial, we will go over the fundamental HelloWorld Program.
+/*
+Unit 1- Lesson 1
+Welcome to Java!
+Java is a powerful coding language and enables humans to talk with a machine.
+In this tutorial, we will go over the introductory HelloWorld Program.
+*/
 
 public class HelloWorld
 {
-  public static void main(String[] args)
+  public static void main(String[] args) //Don't worry about this syntax for now!  We will cover this in later units.
       {
-        // Prints "Hello World" to the terminal window.
-        System.out.println("Hello World!") //Within the parenthesis and quotations, you can change the the phrase to display whatever you want!  Try saying "Hello World!  My name is [...]"
+        //Prints "Hello World" to the terminal window.
+        System.out.println("Hello World!")
+        //Within the parenthesis and quotations, you can change the the phrase to display whatever you want!  Try saying "Hello World!  My name is [...]"
         }
  }
 
