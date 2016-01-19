d893e5ca353beeb8dba47943abfb1950c345fc05
Derek Leung <derex.leung@gmail.com>
2016-01-19 18:21:48 -0500

    Created HelloWorld program
    Quite basic executable code.

---
+++ Finch-Robots/Java/Unit-1/HelloWorld.java
@@ -0,0 +1,11 @@
+//Welcome to Java!  Java is a powerful coding language that has many uses from coding apps to robots to medical devices.  In this tutorial, we will go over the fundamental HelloWorld Program.
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+      {
+        // Prints "Hello World" to the terminal window.
+        System.out.println("Hello World!") //Within the parenthesis and quotations, you can change the the phrase to display whatever you want!  Try saying "Hello World!  My name is [...]"
+        }
+ }
+
