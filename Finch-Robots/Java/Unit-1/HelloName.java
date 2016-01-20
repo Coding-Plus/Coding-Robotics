cccfe62debc45380e2aa8958d8cff8369bcd701f
Derek Leung <derex.leung@gmail.com>
2016-01-19 23:02:07 -0500

    Update HelloName.java
    Added lesson and explanations

--- Finch-Robots/Java/Unit-1/HelloName.java
+++ Finch-Robots/Java/Unit-1/HelloName.java
@@ -1,9 +1,16 @@
+/*
+Unit 1-Lesson 2
+You just did the HelloWorld.java program and saw how it can print something.
+In this lesson, you will see and implement the use of arguments so the computer "communicates" with the human.
+Let's go!
+*/
+
 public class HelloName
 {
     public static void main(String[] args)
     {
-        System.out.print("Hi, ");
-        System.out.print(args[0]);
+        System.out.print("Hello, ");
+        System.out.print(args[0]); //prompts for your name
         System.out.println(". How are you?");
     }
 }
